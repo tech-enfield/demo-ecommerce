93cdb78c91b95ae80318c3389509f96ad947288b
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-12-22 15:16:24 +0545

    changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Middleware\AdminAuthMiddleware;
-use App\Http\Middleware\APIAuthMiddleware;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -21,8 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'admin.auth' => AdminAuthMiddleware::class,
-            'api.auth' => APIAuthMiddleware::class,
-            // 'role' => RoleMiddleware::class,
+            'role' => RoleMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
