7dc7c22725a28f11c67f7ea1430622539e04d25f
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2026-01-10 14:17:22 +0545

    change

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Models\Banner;
+use App\Models\Category;
 use App\Models\ProductVariant;
 
 class ApiController extends BaseController
@@ -12,6 +13,7 @@ class ApiController extends BaseController
         $data['banners'] = Banner::where('status', 1)->orderBy('order', 'asc')->get(['title', 'path', 'hyper_link']);
         $data['featured_products'] = ProductVariant::with(['product'])->where('is_featured', 1)->orderBy('updated_at', 'asc')->limit(10)->get();
         $data['best_sellings'] = ProductVariant::where('is_best_selling', 1)->orderBy('updated_at', 'asc')->limit(10)->get();
+        $data['categories'] = Category::all();
         return $this->sendResponse($data);
     }
 }
