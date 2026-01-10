91f1474d7f5809cf0ca356a29aebc6988cc45276
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2026-01-10 15:38:02 +0545

    changes

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -10,9 +10,9 @@ class ApiController extends BaseController
 {
     public function home() {
         $data['base_url'] = config('app.url');
-        $data['banners'] = Banner::where('status', 1)->orderBy('order', 'asc')->get(['title', 'path', 'hyper_link']);
-        $data['featured_products'] = ProductVariant::with(['product'])->where('is_featured', 1)->orderBy('updated_at', 'asc')->limit(10)->get();
-        $data['best_sellings'] = ProductVariant::where('is_best_selling', 1)->orderBy('updated_at', 'asc')->limit(10)->get();
+        // $data['banners'] = Banner::where('status', 1)->orderBy('order', 'asc')->get(['title', 'path', 'hyper_link']);
+        // $data['featured_products'] = ProductVariant::with(['product'])->where('is_featured', 1)->orderBy('updated_at', 'asc')->limit(10)->get();
+        // $data['best_sellings'] = ProductVariant::where('is_best_selling', 1)->orderBy('updated_at', 'asc')->limit(10)->get();
         $data['categories'] = Category::get(['id','name', 'icon']);
         return $this->sendResponse($data);
     }
