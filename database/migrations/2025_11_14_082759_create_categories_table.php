08d2d66c298795f59ca077b2edca3d2f76ad4d7b
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2026-01-10 15:39:54 +0545

    changes

--- database/migrations/2025_11_14_082759_create_categories_table.php
+++ database/migrations/2025_11_14_082759_create_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('slug')->unique();
+            $table->string('icon')->nullable();
             $table->foreignId('parent_id')->nullable()->constrained('categories')->onDelete('cascade');
             $table->timestamps();
         });
