5711186bd15e37fb64aa08ebe530ed10f7c8f5ac
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2026-01-21 00:38:59 +0545

    changes

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -71,7 +71,7 @@ class ApiController extends BaseController
             'brand:id,name'
         ])
             ->where('is_active', true)
-            ->paginate(20);
+            ->paginate(4);
 
         // Transform paginated collection
         $products->getCollection()->transform(function ($product) {
