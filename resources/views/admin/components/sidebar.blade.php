e0b13e5f222d8770c925af5c02a2de31a71d9777
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2026-02-08 20:19:39 +0545

    changes

--- resources/views/admin/components/sidebar.blade.php
+++ resources/views/admin/components/sidebar.blade.php
@@ -245,9 +245,9 @@
                     <li>
                         <a href="{{ route('admin.orders.index') }}"
                             class="menu-item group
-        {{ request()->is('admin/orders/*') ? 'menu-item-active' : 'menu-item-inactive' }}">
+        {{ request()->is('admin/orders') ? 'menu-item-active' : 'menu-item-inactive' }}">
                             <svg class="menu-item-icon group
-        {{ request()->is('admin/orders/*') ? 'menu-item-icon-active' : 'menu-item-icon-inactive' }}"
+        {{ request()->is('admin/orders') ? 'menu-item-icon-active' : 'menu-item-icon-inactive' }}"
                                 width="24" height="24" viewBox="0 0 24 24" fill="none"
                                 xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" clip-rule="evenodd"
